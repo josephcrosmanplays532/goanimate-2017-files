8025c06024a6e3d0abf0d7368e2e12ae9970825c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-02-20 20:40:05 -0500

    Create email.js

---
+++ js/email.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.emailUtil=(function(){var b=/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,a=function(c){return b.test(c)};return{isValidFormat:a}}());
+
+}
+/*
+     FILE ARCHIVED ON 06:07:00 Jan 01, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:39:48 Feb 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 738.67
+  exclusion.robots: 547.475
+  exclusion.robots.policy: 547.46
+  xauthn.identify: 471.985
+  xauthn.chkprivs: 75.177
+  RedisCDXSource: 4.851
+  esindex: 0.012
+  LoadShardBlock: 156.717 (3)
+  PetaboxLoader3.datanode: 176.798 (5)
+  CDXLines.iter: 26.083 (3)
+  load_resource: 190.504 (2)
+  PetaboxLoader3.resolve: 111.474 (2)
+*/
