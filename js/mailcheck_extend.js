4fa77f6c435a9310e77def109612103409648a6f
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-02-20 20:45:06 -0500

    Create mailcheck_extend.js

---
+++ js/mailcheck_extend.js
@@ -0,0 +1,38 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+Mailcheck.defaultTopLevelDomains.push("co.in","com.hk");
+
+}
+/*
+     FILE ARCHIVED ON 06:07:07 Jan 01, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:44:32 Feb 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  CDXLines.iter: 24.528 (3)
+  load_resource: 212.074 (2)
+  PetaboxLoader3.datanode: 140.685 (5)
+  captures_list: 181.979
+  xauthn.identify: 69.803
+  esindex: 0.015
+  exclusion.robots: 117.959
+  LoadShardBlock: 35.109 (3)
+  PetaboxLoader3.resolve: 62.954 (2)
+  xauthn.chkprivs: 47.691
+  exclusion.robots.policy: 117.943
+  RedisCDXSource: 0.755
+*/
